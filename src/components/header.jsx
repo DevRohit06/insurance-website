55b6d8ea0971f71934cbf7a8d7f7ddba7daa9bd1
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-10-28 22:40:26 +0530

    navbar and hero section

---
+++ src/components/header.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+
+export default function Header() {
+  const [mobile, setMobile] = useState(false);
+  function openMenu(){
+    setMobile(!mobile);
+  }
+  const Links = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Insurance",
+      link: "/insurance",
+    },
+    {
+      name: "About us",
+      link: "/about",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
+  return (
+    <>
+      <header className="w-5/6 mx-auto py-4 relative">
+        <nav className="w-full flex items-center gap-x-2">
+          <div className="w-full flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-bold">LifeInsureEase</h1>
+            </div>
+            <div className="hidden lg:block">
+              <ul className="flex space-x-4">
+                {Links.map((item, index) => {
+                  return <li key={index}>{item.name}</li>;
+                })}
+              </ul>
+            </div>
+            <div className="hidden lg:block">
+              <a
+                className="px-4 py-2.5 bg-primary rounded-md text-white"
+                aria-label="contact button"
+                href="/contact"
+              >
+                Contact Us
+              </a>
+            </div>
+          </div>
+          <div
+            onClick={() => {
+              openMenu();
+            }}
+            className="flex h-9 w-9 p-1.5 bg-primary rounded-xl stroke-white bg-opacity-50 lg:hidden "
+          >
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <g id="Menu / Menu_Alt_02">
+                  {" "}
+                  <path
+                    id="Vector"
+                    d="M11 17H19M5 12H19M11 7H19"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>{" "}
+                </g>{" "}
+              </g>
+            </svg>
+          </div>
+        </nav>
+
+        {mobile ? (
+          <div className=" absolute w-full block lg:hidden">
+            <ul className="text-center bg-white rounded-xl mt-2 shadow-lg px-4 py-4">
+              {Links.map((item, index) => {
+                return (
+                  <li key={index} className=" py-2 hover:shadow-xl rounded-xl">
+                    {item.name}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : null}
+      </header>
+    </>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"primary": "#4F9D69",
+			}
+		},
+	},
+	plugins: [],
+}
